a16199a5019e5bfb1c8c6fc5d7012bfece339043
wgs11 <4teatoo@gmail.com>
2018-03-09 15:38:18 -0500

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,56 @@
 Donation.delete_all
 User.delete_all
 Game.delete_all
-
-
-Heros = [
-    [49, 91, "Ana", "Ana.png"],
-    [57, 90, "Bastion", "Bastion.png"],
-    [57, 91, "D.Va", "DVA.png"],
-    [39, 75, "Doomfist", "Doomfist.png"]
-]
+Category.delete_all
 
 Games = [
     ["Super Mario 64", "sm64logo.png"],
     ["Super Mario Sunshine", "smslogo.png"],
-    ["Super Meat Boy", "smb.png"]
+    ["Super Meat Boy", "smb.png"],
+    ["Paper Mario 64", "papermario.png"]
 ]
 
+Categories = [
+    ["Super Mario 64", "16 Star", 1050, "6-agF6rcktM"],
+    ["Super Mario 64", "70 Star", 3390, "O-d1ponqdOk"],
+    ["Paper Mario 64", "Glitchless", 15273, "ZKDw0Yu-6-U"]
+]
+
+Incentives = [
+    [0, 301, "Super Mario 602", "smslogo.png"],
+    [97.85, 250, "Paper Mario TTYD 100%", "ttyd.jpg"],
+    [0, 200, "SM64 700 Star Run", "sm64logo.png"],
+    [21, 150, "Wind Waker Any%", "ww.png"],
+    [0.1, 150, "Paper Mario 64 100%", "papermario.png"],
+    [120, 120, "24 Hour Stream", "smslogo.png"],
+    [1, 100, "SMS 420", "smslogo.png"],
+    [0, 100, "SMS All Categories", "smslogo.png"],
+    [0, 100, "LoZ: Ocarina of Time Any% Glitchless", "oot.png"],
+    [0, 100, "LoZ: Majora's Mask Any% Glitchless", "mm.png"],
+    [0, 100, "LoZ: Majora's Mask All Masks", "mm.png"],
+    [100, 100, "Super Mario Galaxy 2 242", "smglogo.png"],
+    [75, 75, "Drinking Stream", "smslogo.png"],
+    [75, 75, "Super Mario 64 Chaos 70 Star", "sm64logo.png"],
+    [0, 65, "Paper Mario TTYD Any%", "ttyd.jpg"],
+    [65, 65, "Shovel Knight Any%", "smslogo.png"],
+    [0, 45, "Paper Mario 64 Glitchless", "papermario.png"],
+    [16, 30, "Pikmin All Parts", "Pikmin_logo.png"],
+    [0, 30, "Pikmin 2 Pay off Debt", "Pikmin_logo.png"],
+    [30, 30, "Pikmin 2 All Treasures", "Pikmin_logo.png"],
+    [0.21, 25, "Pokemon Red/Blue Glitchless", "red.png"],
+    [25, 25, "Perler Giveaway", "smslogo.png"],
+    [3, 35, "Spyro Year of the Dragon 117%", "spyro.png"],
+    [30, 30, "Super Meat Boy Any%", "smb.png"],
+    [30, 30, "Chubby Bunny", "smslogo.png"],
+    [0, 100, "Paper Mario TTYD 'I Love Yous'", "ttyd.jpg"]
+]
 Games.each do |name, image_url|
   Game.create(name: name, image_url: image_url)
 end
 
-
-Incentives = [
-    [0, 301, "Super Mario 602", "smslogo.png"],
-    [97.85, 250, "Paper Mario TTYD 100%", "smslogo.png"],
-    [0, 200, "SM64 700 Star Run", "smslogo.png"],
-    [6, 150, "Wind Waker Any%", "smslogo.png"],
-    [0.1, 150, "Paper Mario 64 100%", "smslogo.png"],
-    [120, 120, "24 Hour Stream", "smslogo.png"],
-    [1, 100, "SMS 420", "smslogo.png"],
-    [0, 100, "SMS All Categories", "smslogo.png"],
-    [0, 100, "LoZ: Ocarina of Time Any% Glitchless", "smslogo.png"],
-    [0, 100, "LoZ: Majora's Mask Any% Glitchless", "smslogo.png"],
-    [0, 100, "LoZ: Majora's Mask All Masks", "smslogo.png"],
-    [100, 100, "Super Mario Galaxy 2 242", "smslogo.png"],
-    [75, 75, "Drunk Stream", "smslogo.png"],
-    [48, 75, "Super Mario 64 Chaos 70 Star", "smslogo.png"],
-    [0, 65, "Paper Mario TTYD Any%", "smslogo.png"],
-    [65, 65, "Shovel Knight Any%", "smslogo.png"],
-    [0, 45, "Paper Mario 64 Glitchless", "smslogo.png"],
-    [16, 30, "Pikmin All Parts", "smslogo.png"],
-    [0, 30, "Pikmin 2 Pay off Debt", "smslogo.png"],
-    [30, 30, "Pikmin 2 All Treasures", "smslogo.png"],
-    [0.21, 25, "Pokemon Red/Blue Glitchless", "smslogo.png"],
-    [25, 25, "Perler Giveaway", "smslogo.png"],
-    [3, 35, "Spyro Year of the Dragon 117%", "smslogo.png"],
-    [30, 30, "Super Meat Boy Any%", "smslogo.png"],
-    [30, 30, "Chubby Bunny", "smslogo.png"],
-    [0, 100, "Paper Mario TTYD 'I Love Yous'", "smslogo.png"]
-]
+Categories.each do |game_id, cat_name, time, video_url|
+  Category.create(game_id: game_id, cat_name: cat_name, time: time, video_url: video_url)
+end
 
 Incentives.each do |progress, total, incentive, image_url|
   Donation.create(progress: progress, total: total, incentive: incentive, image_url: image_url)
